c18efb9bda10f6841f3ae847bba7bd548d33ea94
Vinicius Mendite <vinicius.miff@gmail.com>
2020-04-30 15:31:58 -0300

    Card concluído

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 
+import attachMoney from '../assets/material-icons/dolar.png';
+import visibilityOff from '../assets/material-icons/money-off.png';
+
 import Header from '../components/Header';
 import Tabs from '../components/Tabs';
-import {Container} from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  Icon,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <StatusBar barStyle="light-content" backgroundColor="#8b10ae" />
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon source={attachMoney} />
+            <Icon source={visibilityOff} />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197.611,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Tranferência de R$ 100,00 recebida de Vinícius Mendite hoje às
+              08:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
