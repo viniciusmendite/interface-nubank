cb1ef3c68054713543a3aae642419a7b4c4b9808
Vinicius Mendite <vinicius.miff@gmail.com>
2020-04-30 14:49:34 -0300

    terminei as tabs

--- src/Main/index.js
+++ src/Main/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 
 import Header from '../components/Header';
+import Tabs from '../components/Tabs';
 import {Container} from './styles';
 
 export default function Main() {
@@ -9,6 +10,7 @@ export default function Main() {
     <Container>
       <StatusBar barStyle="light-content" backgroundColor="#8b10ae" />
       <Header />
+      <Tabs />
     </Container>
   );
 }
--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
-import {Container} from './styles';
+import {Container, TabsContainer, TabItem, Icon, TabText} from './styles';
+import personAdd from '../../assets/material-icons/person-add.png';
+import chatBuble from '../../assets/material-icons/chat-buble.png';
+import arrowDownWard from '../../assets/material-icons/arrow-downward.png';
+import arrowUpWard from '../../assets/material-icons/arrow-upward.png';
+import lock from '../../assets/material-icons/lock.png';
 
 export default function Tabs() {
-  return <Container />;
+  return (
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon source={personAdd} />
+          <TabText>Indicar Amigos</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon source={chatBuble} />
+          <TabText>Cobrar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon source={arrowDownWard} />
+          <TabText>Depositar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon source={arrowUpWard} />
+          <TabText>Transferir</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon source={lock} />
+          <TabText>Bloquear Cartão</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
 }
